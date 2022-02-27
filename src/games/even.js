39426b22db5efec19c3134d23fe3138ebae960d0
Antoxa63 <63larionov@gmail.com>
2022-02-27 11:41:58 +0400

    brain-even

---
+++ src/games/even.js
@@ -0,0 +1,14 @@
+import getRandomNumber from '../common.js';
+import newGame from '../index.js';
+
+const isEven = (number) => number % 2 === 0;
+const description = 'Answer "yes" if the number is even, otherwise answer "no".';
+const generateQuestionAnswer = () => {
+  const fromNumber = 0;
+  const question = getRandomNumber(fromNumber);
+  const answer = isEven(question) ? 'yes' : 'no';
+  return [question, answer];
+};
+const brainEven = () => newGame(description, generateQuestionAnswer);
+
+export default brainEven;
